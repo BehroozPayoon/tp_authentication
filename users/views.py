825e5d00b0cff3785933b5a7d2d34d6c742a7836
Behrooz Payoon <behrooz.payoon@gmail.com>
2024-07-30 16:15:31 +0330

    Add endpoint to authenticate user with sent access token

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 from .serializers import RegisterSerializer, UserSerializer
 
@@ -24,3 +26,14 @@ class LoginView(generics.GenericAPIView):
                 'access': str(refresh.access_token),
             })
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthenticateView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        token = request.data.get('token')
+        try:
+            AccessToken(token)
+            return Response({'valid': True})
+        except (TokenError, InvalidToken):
+            return Response({'valid': False}, status=status.HTTP_400_BAD_REQUEST)
